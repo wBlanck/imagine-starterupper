94463a3eae5c1349dcfdb3a6349eb09ca5cca09b
wblanck - laptop <blanck.william@gmail.com>
2021-11-11 11:26:34 +0100

    added mobile menu

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ window.onscroll = () => {
     document.getElementById('top-header').classList.remove('header--shrink');
   }
 }
+
+let hamburger = document.querySelector(".hamburger");
+let closeMobile = document.querySelector(".close");
+let mobileNav = document.querySelector(".mobile-nav");
+
+hamburger.addEventListener('click', () => {
+  mobileNav.style.left = "0";
+})
+closeMobile.addEventListener('click', () => {
+  mobileNav.style.left = "100%";
+})
