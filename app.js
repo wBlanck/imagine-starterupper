cf55115c46efde01c95851c6cf68dc88b9e60aae
wblanck <blanck.william@gmail.com>
2021-12-24 16:38:38 +0100

    added GSAP lib

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 window.onscroll = () => {
-  if(window.pageYOffset > 50) {
-    document.getElementById('top-header').classList.add('header--shrink');
+  if (window.pageYOffset > 50) {
+    document.getElementById("top-header").classList.add("header--shrink");
   } else {
-    document.getElementById('top-header').classList.remove('header--shrink');
+    document.getElementById("top-header").classList.remove("header--shrink");
   }
-}
+};
 
 let hamburger = document.querySelector(".hamburger");
 let closeMobile = document.querySelector(".close");
 let mobileNav = document.querySelector(".mobile-nav");
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener("click", () => {
   mobileNav.style.left = "0";
-})
-closeMobile.addEventListener('click', () => {
+});
+closeMobile.addEventListener("click", () => {
   mobileNav.style.left = "100%";
-})
+});
